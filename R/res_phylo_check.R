dfc9f8987774b37b3b6104dd70d3c0b243cdd1af
MartinBulla <bulla.mar@gmail.com>
2022-07-04 14:03:33 +0200

    Update res_phylo_check.R

--- R/res_phylo_check.R
+++ R/res_phylo_check.R
@@ -1,8 +1,9 @@
-####|Testing for phylo signal in residuals of 
+####|Testing for phylo signal in residuals of period (Table S1 - 1d) and stringency index models (Table S2 - 1c)
 
 # packages and data
     library(ape)
     library(arm)
+    library(brms)
     library(coda)
     library(dplyr)
     library(geiger)
@@ -40,8 +41,8 @@
     inv.phylo_s <- inverseA(tree_fids, nodes="ALL", scale=TRUE)
     phyloress <-solve(inv.phylo_s$Ainv)
     rownames(phyloress) <-rownames(inv.phylo_s$Ainv)
-
-#MCMC phylogenetic models
+ 
+# MCMC phylogenetic models
   nitt = 1000000
   burnin=20000
   thin=1000
@@ -70,18 +71,54 @@
 
 
   # period
-    mcmc2<-MCMCglmm(res ~ 1, random=~Species, family="gaussian", ginverse=list(Species = inv.phylo_d$Ainv), 
-                    verbose=F, nitt=nitt, burnin=burnin, thin=thin, data=d)
+    # using Jarrod's prior - improves posterior distribution of Species
+        # an inverse-gamma prior for the residual variance with shape and scale equal to 0.001 was used for the residual variance (that is, variance set to 1 and the degree of belief parameter to 0.002).
+        # the parameter-expanded priors for Species to give scaled F-distributions with numerator and denominator degrees of freedom set to 1 and a scale parameter of 1,000.
+        prior_Jarrod<-list(R=list(V=1,nu=0.002),
+                                    G=list(
+                                        G0=list(V=1,nu=1, alpha.mu=0, alpha.V=1000)#,
+                                        #G3=list(V=1,nu=0.002)                                      
+                                        ))  
+        mcmc2_2<-MCMCglmm(res ~ 1, random=~Species, family="gaussian", ginverse=list(Species = inv.phylo_d$Ainv), 
+                    verbose=F, nitt=nitt, burnin=burnin, thin=thin, data=d, prior = prior_Jarrod)
+        plot(mcmc2_2$Sol)
+        plot(mcmc2_2$VCV)
+        plot(log(mcmc2_2$VCV))
+
+        autocorr(mcmc2_2$Sol)
+        autocorr.diag(mcmc2_2$VCV)
+
+        lambda <- mcmc2_2$VCV[,"Species"] / (mcmc2_2$VCV[,"Species"] + mcmc2_2$VCV[,"units"])
+        #summary(lambda2)
+        l[['d']] = data.table(model = 'Table S1 - 1d', lambda = posterior.mode(lambda), lwr = HPDinterval(lambda, 0.95)[1], upr = HPDinterval(lambda, 0.95)[2])
     
-    plot(mcmc2$Sol)
-    plot(mcmc2$VCV)
+    # using in-build prior leads to poor posterior distribution of Species
+        mcmc2<-MCMCglmm(res ~ 1, random=~Species, family="gaussian", ginverse=list(Species = inv.phylo_d$Ainv), 
+                        verbose=F, nitt=nitt, burnin=burnin, thin=thin, data=d)
 
-    autocorr(mcmc2$Sol)
-    autocorr.diag(mcmc2$VCV)
+        plot(mcmc2$Sol)
+        plot(mcmc2$VCV)
+        plot(log(mcmc2$VCV))
 
-    #summary(mcmc2)
-    lambda <- mcmc2$VCV[,"Species"] / (mcmc2$VCV[,"Species"] + mcmc2$VCV[,"units"])
-    #summary(lambda2)
-    l[['d']] = data.table(model = 'Table S1 - 1d', lambda = posterior.mode(lambda), lwr = HPDinterval(lambda, 0.95)[1], lwr = HPDinterval(lambda, 0.95)[2])
+        autocorr(mcmc2$Sol)
+        autocorr.diag(mcmc2$VCV)
 
+        #summary(mcmc2)
+        #lambda <- mcmc2$VCV[,"Species"] / (mcmc2$VCV[,"Species"] + mcmc2$VCV[,"units"])
+        #summary(lambda2)
+        #l[['d']] = data.table(model = 'Table S1 - 1d', lambda = posterior.mode(lambda), lwr = HPDinterval(lambda, 0.95)[1], lwr = HPDinterval(lambda, 0.95)[2])
   do.call(rbind, l)  
+
+# compare model with and without phylogeny
+    mcmc2_no = MCMCglmm(res ~ 1, random=~Species, family="gaussian",
+                    verbose=F, nitt=nitt, burnin=burnin, thin=thin, data=d)
+    mcmc2_no$DIC-mcmc2$DIC # 0.02710599
+
+    mcmc1_no = MCMCglmm(res ~ 1, random=~Species, family="gaussian",  
+                    verbose=F, nitt=nitt, burnin=burnin, thin=thin, data=s)
+    mcmc1_no$DIC-mcmc1$DIC # -10.94093
+# export
+  save(file = 'Data/DAT_mcmc.Rdata', mcmc1_no, mcmc1, mcmc2_no, mcmc2)
+
+# End   
+# END
