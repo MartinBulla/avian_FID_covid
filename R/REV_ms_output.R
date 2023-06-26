3c8375a29aa5de6982e5a6b63f7db4d89b2bb153
MartinBulla <bulla.mar@gmail.com>
2023-06-26 14:50:46 +0200

    updated phylo tests

--- R/REV_ms_output.R
+++ R/REV_ms_output.R
@@ -3006,12 +3006,387 @@ ggsave(here::here("Outputs/Fig_S7_width-152mm_Google.png"), gtgx2, width = 15.24
 #' **Figure S8 | Species-specific avian tolerance towards humans in relation to Google Mobility.**</a> Each dot represents a single escape distance observation (not corrected for other factors such as starting distance of the observer) and a day-specific value of Google Mobility for parks in a given country. Dot colour highlights the country. Grey lines represent locally weighted smoothing, a non-parametric local regression fitted with the ggplot function of ggplot2 package (Wickham 2016), highlighting heterogenous (and usually unclear – close to zero) within- and between- species trends. The y-axes is on the log-scale. Some species lack trend lines because data distribution hindered the smoothing and visualised are only data for species with ≥10 escape distance observations.
 #'
 #' ***
+#' 
+#' ***
+#' ### Testing for phylo-signal in residuals
+#' Example code. Don't run, as it takes too long. To make the Table S5, load the saved mcmc outputs instead.
+#+ prep_phylo, eval = FALSE, results = 'hide', warning=FALSE
+# packages and data
+library(ape)
+library(brms)
+library(coda)
+library(dplyr)
+library(geiger)
+library(parallel)
+library(phangorn)
+library(phylobase)
+
+sapply(c(
+  "magrittr", "car",
+  "ape", "phytools",  "stringr", "plyr"
+),
+require,
+character.only = TRUE
+)
+
+trees = read.tree("Data/trees.tre")
+
+d[, scinam := Species]
+
+s[, scinam := Species]
+
+
+# get residuals
+# period
+ms <- lmer(scale(log(FID)) ~
+  scale(log(SD)) +
+  scale(log(FlockSize)) +
+  scale(log(BodyMass)) +
+  scale(sin(rad)) + scale(cos(rad)) +
+  # scale(Day)+
+  scale(Temp) +
+  scale(Covid) +
+  (1 | Year) + (1 | weekday) + (1 | genus) + (1 | Species) + (1 | sp_day_year) + (scale(Covid) | Country) + (scale(Covid) | IDLocality) + (1 | sp_loc),
+data = d, REML = FALSE,
+control <- lmerControl(
+  optimizer = "optimx", optCtrl = list(method = "nlminb")
+)
+)
+d[, res_period := resid(ms)]
+# stringency
+m01a <- lmer(scale(log(FID)) ~
+  scale(Year) +
+  scale(log(SD)) +
+  scale(log(FlockSize)) +
+  scale(log(BodyMass)) +
+  scale(sin(rad)) + scale(cos(rad)) +
+  # scale(Day)+
+  scale(Temp) +
+  scale(StringencyIndex) +
+  (1 | weekday) + (1 | genus) + (1 | Species) + (1 | sp_day_year) + (scale(StringencyIndex) | Country) + (1 | IDLocality) + (1 | sp_loc),
+data = s, REML = FALSE
+)
+s[, res := resid(m01a)]
+# google
+ss = s[!is.na(parks_percent_change_from_baseline)]
+ss[, country_year := paste(Country, Year)] # table(paste(s$Country, s$Year))
+ss[parks_percent_change_from_baseline < 0, google := "before_zero"]
+ss[parks_percent_change_from_baseline > 0, google := "after_zero"]
+ss[, sp_country_google := paste(sp_country, google)]
+mg01a = lmer(scale(log(FID)) ~
+  scale(Year) +
+  scale(log(SD)) +
+  scale(log(FlockSize)) +
+  scale(log(BodyMass)) +
+  scale(sin(rad)) + scale(cos(rad)) +
+  # scale(Day)+
+  scale(Temp) +
+  scale(parks_percent_change_from_baseline) +
+  (1 | weekday) + (1 | genus) + (1 | Species) + (1 | sp_day_year) + (scale(parks_percent_change_from_baseline) | Country) + (1 | IDLocality) + (1 | sp_loc),
+data = ss, REML = FALSE
+)
+ss[, res := resid(mg01a)]
+# MCC tree and covariance matrix
+namesd <- d %>% distinct(Species)
+row.names(namesd) <- namesd$Species
+namecheck_fidd <- name.check(trees[[1]], namesd)
+trees_fidd <- lapply(trees, drop.tip, namecheck_fidd$tree_not_data)
+class(trees_fidd) <- "multiPhylo"
+length(trees_fidd[[1]]$tip.label)
+tree_fidd <- maxCladeCred(trees_fidd)
+tree_fidd$node.label <- NULL
+inv.phylo_d <- inverseA(tree_fidd, nodes = "ALL", scale = TRUE)
+phyloresd <- solve(inv.phylo_d$Ainv)
+rownames(phyloresd) <- rownames(inv.phylo_d$Ainv)
+
+namess <- s %>% distinct(Species)
+row.names(namess) <- namess$Species
+namecheck_fids <- name.check(trees[[1]], namess)
+trees_fids <- lapply(trees, drop.tip, namecheck_fids$tree_not_data)
+class(trees_fids) <- "multiPhylo"
+length(trees_fids[[1]]$tip.label)
+tree_fids <- maxCladeCred(trees_fids)
+tree_fids$node.label <- NULL
+inv.phylo_s <- inverseA(tree_fids, nodes = "ALL", scale = TRUE)
+phyloress <- solve(inv.phylo_s$Ainv)
+rownames(phyloress) <- rownames(inv.phylo_s$Ainv)
+
+namesss <- ss %>% distinct(Species)
+row.names(namesss) <- namesss$Species
+namecheck_fidss <- name.check(trees[[1]], namesss)
+trees_fidss <- lapply(trees, drop.tip, namecheck_fidss$tree_not_data)
+class(trees_fidss) <- "multiPhylo"
+length(trees_fidss[[1]]$tip.label)
+tree_fidss <- maxCladeCred(trees_fidss)
+tree_fidss$node.label <- NULL
+inv.phylo_ss <- inverseA(tree_fidss, nodes = "ALL", scale = TRUE)
+phyloresss <- solve(inv.phylo_ss$Ainv)
+rownames(phyloresss) <- rownames(inv.phylo_ss$Ainv)
+
+# brms
+
+# period - no - need to run (takes time), if you load the above DAT_brms.Rdata
+# without phylo
+priors <- get_prior(res_period ~ 0 + Intercept + (1 | Species), data = d) #
+mP_no = brm(
+  form = res_period ~ 0 + Intercept + (1 | Species),
+  data = d,
+  cores = 2,
+  chains = 5,
+  control = list(adapt_delta = 0.99),
+  iter = 5000,
+  thin = 5,
+  sample_prior = "yes",
+  save_pars = save_pars(all = TRUE),
+  prior = priors,
+  seed = 5
+)
+plot(mP_no, ask = FALSE)
+pp_check(mP_no, ndraws = 100)
+mcmc_plot(mP_no, type = "acf")
+summary(mP_no)
+# with phylo
+priors <- get_prior(res_period ~ 0 + Intercept + (1 | Species) + (1 | gr(scinam, cov = A)), data = d, data2 = list(A = phyloresd))
+mP_yes = brm(
+  form = res_period ~ 0 + Intercept + (1 | Species) + (1 | gr(scinam, cov = A)),
+  data = d,
+  data2 = list(A = phyloresd),
+  cores = 2,
+  chains = 5,
+  control = list(adapt_delta = 0.99),
+  iter = 5000,
+  thin = 5,
+  sample_prior = "yes",
+  save_pars = save_pars(all = TRUE),
+  prior = priors,
+  seed = 5
+)
+plot(mP_yes, ask = FALSE)
+pp_check(mP_yes, ndraws = 100)
+# pp_check(m_yes, type = "scatter_avg_grouped", group = "Species") +  geom_abline(intercept = 0, slope = 1 , color = "red", lty = 2)
+mcmc_plot(mP_yes, type = "acf")
+summary(mP_yes)
+
+# lambda
+hypothesis(mP_yes, "sd_scinam__Intercept^2 / (sd_scinam__Intercept^2 + sd_Species__Intercept^2) = 0", class = NULL)
+hypothesis(mP_yes, "sd_scinam__Intercept^2 / (sd_scinam__Intercept^2 + sd_Species__Intercept^2 + sigma^2) = 0", class = NULL)
+
+
+v_sc <- (VarCorr(mP_yes, summary = FALSE)$scinam$sd)^2
+v_sp <- (VarCorr(mP_yes, summary = FALSE)$Species$sd)^2
+v_r <- (VarCorr(mP_yes, summary = FALSE)$residual$sd)^2
+summary(as.mcmc(v_sc / (v_sc + v_sp + v_r)))
+
+# compare period models
+# 1. LOOic
+loo(mP_yes)
+loo(mP_no)
+
+# 2. Bayes factor
+bayes_factor(mP_no, mP_yes)
+
+# 3. Poterior probability
+post_prob(mP_yes, mP_no)
+
+# stringency index  - no - need to run (takes time), if you load the above DAT_brms.Rdata
+# without phylo
+priors <- get_prior(res ~ 0 + Intercept + (1 | Species), data = s) #
+m_no = brm(
+  form = res ~ 0 + Intercept + (1 | Species),
+  data = s,
+  cores = 2,
+  chains = 5,
+  control = list(adapt_delta = 0.99),
+  iter = 5000,
+  thin = 5,
+  sample_prior = "yes",
+  save_pars = save_pars(all = TRUE),
+  prior = priors,
+  seed = 5
+)
+plot(m_no, ask = FALSE)
+pp_check(m_no, ndraws = 100)
+mcmc_plot(m_no, type = "acf")
+summary(m_no)
+# with phylo
+priors <- get_prior(res ~ 0 + Intercept + (1 | Species) + (1 | gr(scinam, cov = A)), data = s, data2 = list(A = phyloress))
+m_yes = brm(
+  form = res ~ 0 + Intercept + (1 | Species) + (1 | gr(scinam, cov = A)),
+  data = s,
+  data2 = list(A = phyloress),
+  cores = 2,
+  chains = 5,
+  control = list(adapt_delta = 0.99),
+  iter = 5000,
+  thin = 5,
+  sample_prior = "yes",
+  save_pars = save_pars(all = TRUE),
+  prior = priors,
+  seed = 5
+)
+plot(m_yes, ask = FALSE)
+pp_check(m_yes, ndraws = 100)
+# pp_check(m_yes, type = "scatter_avg_grouped", group = "Species") +  geom_abline(intercept = 0, slope = 1 , color = "red", lty = 2)
+mcmc_plot(m_yes, type = "acf")
+summary(m_yes)
+
+# lambda
+hypothesis(m_yes, "sd_scinam__Intercept^2 / (sd_scinam__Intercept^2 + sd_Species__Intercept^2) = 0", class = NULL) # lambda
+hypothesis(m_yes, "sd_scinam__Intercept^2 / (sd_scinam__Intercept^2 + sd_Species__Intercept^2 + sigma^2) = 0", class = NULL)
+
+v_sc <- (VarCorr(m_yes, summary = FALSE)$scinam$sd)^2
+v_sp <- (VarCorr(m_yes, summary = FALSE)$Species$sd)^2
+v_r <- (VarCorr(m_yes, summary = FALSE)$residual$sd)^2
+summary(as.mcmc(v_sc / (v_sc + v_sp + v_r)))
+
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+Mode(as.mcmc(v_sc / (v_sc + v_sp + v_r)))
+
+# compare stringency models
+# 1. LOOic
+loo(m_yes)
+loo(m_no)
+
+# 2. Bayes factor
+bayes_factor(m_no, m_yes)
+
+# 3. Poterior probability
+post_prob(m_yes, m_no)
+
+# Google Mobility  - no - need to run (takes time), if you load the above DAT_brms.Rdata
+# without phylo
+priors <- get_prior(res ~ 0 + Intercept + (1 | Species), data = ss) #
+mG_no = brm(
+  form = res ~ 0 + Intercept + (1 | Species),
+  data = ss,
+  cores = 2,
+  chains = 5,
+  control = list(adapt_delta = 0.99),
+  iter = 5000,
+  thin = 5,
+  sample_prior = "yes",
+  save_pars = save_pars(all = TRUE),
+  prior = priors,
+  seed = 5
+)
+plot(mG_no, ask = FALSE)
+pp_check(mG_no, ndraws = 100)
+mcmc_plot(mG_no, type = "acf")
+summary(mG_no)
+# with phylo
+priors <- get_prior(res ~ 0 + Intercept + (1 | Species) + (1 | gr(scinam, cov = A)), data = ss, data2 = list(A = phyloress))
+mG_yes = brm(
+  form = res ~ 0 + Intercept + (1 | Species) + (1 | gr(scinam, cov = A)),
+  data = ss,
+  data2 = list(A = phyloress),
+  cores = 2,
+  chains = 5,
+  control = list(adapt_delta = 0.99),
+  iter = 5000,
+  thin = 5,
+  sample_prior = "yes",
+  save_pars = save_pars(all = TRUE),
+  prior = priors,
+  seed = 5
+)
+plot(mG_yes, ask = FALSE)
+pp_check(mG_yes, ndraws = 100)
+# pp_check(m_yes, type = "scatter_avg_grouped", group = "Species") +  geom_abline(intercept = 0, slope = 1 , color = "red", lty = 2)
+mcmc_plot(mG_yes, type = "acf")
+summary(mG_yes)
+
+# lambda
+hypothesis(mG_yes, "sd_scinam__Intercept^2 / (sd_scinam__Intercept^2 + sd_Species__Intercept^2) = 0", class = NULL) # lambda
+hypothesis(mG_yes, "sd_scinam__Intercept^2 / (sd_scinam__Intercept^2 + sd_Species__Intercept^2 + sigma^2) = 0", class = NULL)
+
+v_sc <- (VarCorr(mG_yes, summary = FALSE)$scinam$sd)^2
+v_sp <- (VarCorr(mG_yes, summary = FALSE)$Species$sd)^2
+v_r <- (VarCorr(mG_yes, summary = FALSE)$residual$sd)^2
+summary(as.mcmc(v_sc / (v_sc + v_sp + v_r)))
+
+
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+Mode(as.mcmc(v_sc / (v_sc + v_sp + v_r)))
+
+# compare stringency models
+# 1. LOOic
+loo(mG_yes)
+loo(mG_no)
+
+# 2. Bayes factor
+bayes_factor(mG_no, mG_yes)
+
+# 3. Poterior probability
+post_prob(mG_yes, mG_no)
+# export models
+save(file = 'Data/rev_DAT_brms.Rdata', mP_no, mP_yes, m_no, m_yes, mG_no, mG_yes)
+load(here::here("Data/rev_DAT_brms.Rdata"))
+v_sc <- (VarCorr(mP_yes, summary = FALSE)$scinam$sd)^2
+v_sp <- (VarCorr(mP_yes, summary = FALSE)$Species$sd)^2
+v_r <- (VarCorr(mP_yes, summary = FALSE)$residual$sd)^2
+
+s_v_sc <- (VarCorr(m_yes, summary = FALSE)$scinam$sd)^2
+s_v_sp <- (VarCorr(m_yes, summary = FALSE)$Species$sd)^2
+s_v_r <- (VarCorr(m_yes, summary = FALSE)$residual$sd)^2
+
+g_v_sc <- (VarCorr(mG_yes, summary = FALSE)$scinam$sd)^2
+g_v_sp <- (VarCorr(mG_yes, summary = FALSE)$Species$sd)^2
+g_v_r <- (VarCorr(mG_yes, summary = FALSE)$residual$sd)^2
+# summary(as.mcmc(v_sc / (v_sc + v_sp + v_r)))
+
+ts5 <- data.table(
+  "Residuals\nfrom" = c(
+    "Table S2a - model 01a",
+    "Table S2b - model 01a",
+    "Table S2c - model 01a"
+  ),
+  "Variance explained by phylogeny\n(95%CI)" = c(
+    paste0(paste(round(quantile(as.mcmc(v_sc / (v_sc + v_sp + v_r)), probs = c(0.025, 0.975)) * 100, 2), collapse = "-"), "%"),
+    paste0(paste(round(quantile(as.mcmc(s_v_sc / (s_v_sc + s_v_sp + s_v_r)), probs = c(0.025, 0.975)) * 100, 2), collapse = "-"), "%"),
+    paste0(paste(round(quantile(as.mcmc(g_v_sc / (g_v_sc + g_v_sp + g_v_r)), probs = c(0.025, 0.975)) * 100, 2), collapse = "-"), "%")
+  ),
+  "Bayes\nfactor" = c(
+    round(bayes_factor(mP_no, mP_yes)$bf),
+    round(bayes_factor(m_no, m_yes)$bf),
+    round(bayes_factor(mG_no, mG_yes)$bf)
+  ),
+  "Probability of non-phylogenetic model" = c(
+    round(post_prob(mP_yes, mP_no)[2], 2),
+    round(post_prob(m_yes, m_no)[2], 2),
+    round(post_prob(mG_yes, mG_no)[2], 2)
+  )
+)
+save(file = "Data/T_S5.Rdata", ts5)
+fwrite(file = "Data/T_S5.csv", ts5)
+
+#+t_s5, echo=FALSE,results='hide', warning=FALSE, message=FALSE
+#' <a name="T_S5">
+#' **Table S5 | Comparison of models without and with control for phylogeny.**</a>
+ts5 = fread(here::here('Data/T_S5.csv'))
+ts5 %>%
+  kbl(align=c('l', 'r', 'r','r')) %>%
+  kable_paper("hover", full_width = F)
+#'
+#' **Variance explained by phylogeny (95%CI)** represents percentage of variance explained by phylogeny in an intercept only model fitted to residuals of the original models in STAN (Stan Development Team 2022) using ‘brm’ function from ‘brms’ R-package (Bürkner 2017, Bürkner 2018) with phylogeny and species as random effects. **Bayes factor** in favour of model without phylogeny and **probability of non-phylogenetic model** in comparison to a model with phylogeny. The Gelman-Rubin diagnostics was 1 for all models, indicating model convergence (Brooks & Gelman 1998). Note that for all cases, the model without phylogeny fits residuals better than a model with phylogeny, which justifies our use of simple original models, not controlled for phylogeny.
+# END
 #'
 #' ### References
 #' - Bulla, M., Blumstein, D.T., Benedetti, Y., Floigl, K., Jokimäki, J., Kaisanlahti-Jokimäki, M.-L., Markó, G., Morelli, F., Siretckaia, A., Szakony, S., Weston, M.A., Zeid, F.A., Tryjanowski, P., Albrecht, T. & Mikula, P. (2022). Supporting information for 'Urban birds' flight responses were unaffected by the COVID-19 shutdowns'. Open Science Framework https://doi.org/10.17605/OSF.IO/WUZH7.
+#' - Bürkner PC. (2018). Advanced Bayesian multilevel modeling with the R package brms. R Journal 10:395–411. https://doi.org/10.32614/RJ-2018-017
+#' - Bürkner PC. (2017). brms: An R package for Bayesian multilevel models using Stan. J Stat Softw 80:1–28. https://doi.org/10.18637/jss.v080.i01
 #' - Gelman, A., Su, Y.-S., Yajima, M., Hill, J., Pittau, M., Kerman, J., Zheng, T., & Vincent, D. (2016). Data Analysis using Regression and Multilevel/Hierarchical Models. In CRAN Repository (1.8-6.; pp. 1–53).
 #' - Hale T., Angrist N., Goldszmidt R., Kira B., Petherick A., Phillips T., Webster S., Cameron-Blake E., Hallas L., Majumdar S., Tatlow H. (2021). A global panel database of pandemic policies (Oxford COVID-19 Government Response Tracker). Nature Human Behaviour 2021 5:4 5:529–538. https://doi.org/10.1038/s41562-021-01079-8.
 #' - Peterson, B. G., & Carl, P. (2020). PerformanceAnalytics: Econometric Tools for Performance and Risk Analysis. R package version 2.0.4. https://CRAN.R-project.org/package=PerformanceAnalytics
+#' - Stan Development Team (2022). Stan Modeling Language Users Guide and Reference Manual, Version 2.28. https://mc-stan.org/users/documentation/
 #' - Wickham, H. (2016). ggplot2: Elegant Graphics for Data Analysis. Springer-Verlag, New York.
 #'
 #' ### Session info
