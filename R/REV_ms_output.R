d959d072212f9136af2540d785c9dde9fe75f890
MartinBulla <bulla.mar@gmail.com>
2023-08-05 09:04:27 +0200

    updated links

--- R/REV_ms_output.R
+++ R/REV_ms_output.R
@@ -493,7 +493,7 @@ knitr::opts_chunk$set(message = FALSE, warning = FALSE, cache = TRUE)
 #' ### Introduction
 #' To facilitate transparency, the following document contains example code used to generate the results of the manuscript. Thus, apart from the supplementary figures and tables, below we display also main text figures. The figures and tables are ordered according to their appearance in the main text. The code is displayed upon clicking the "code" icon at the top right, above each display item. Continuous variables were  standardised by subtracting the mean and dividing by the standard deviation. For descriptions of variables see Methods of the [paper](https://doi.org/10.1101/2022.07.15.500232). 'Residual' in tables indicates residual variance.
 #' 
-#' When using this content **PLEASE CITE** the [paper](https://doi.org/10.1101/2022.07.15.500232) and this repository (Bulla et al. 2023, *Supporting information for 'Urban birds’ flight responses were largely unaffected by the COVID-19 shutdowns', GitHub, [https://martinbulla.github.io/avian_FID_covid/](https://martinbulla.github.io/avian_FID_covid/)).
+#' When using this content **PLEASE CITE** the [paper](https://doi.org/10.1101/2022.07.15.500232) and this repository (Martin Bulla, Peter Mikula, Daniel T. Blumstein, Yanina Benedetti, Kristina Floigl, Jukka Jokimäki, Marja-Liisa Kaisanlahti-Jokimäki, Gábor Markó, Federico Morelli, Anders Pape Møller, Anastasiia Siretckaia, Sára Szakony, Michael A. Weston, Farah Abou Zeid, Piotr Tryjanowski & Tomáš Albrecht (2023), *Supporting information for 'Urban birds’ flight responses were largely unaffected by the COVID-19 shutdowns', GitHub, [https://martinbulla.github.io/avian_FID_covid/](https://martinbulla.github.io/avian_FID_covid/)).
 #' 
 #' Questions can be directed to bulla.mar@gmail.com & petomikula158@gmail.com
 #' 
@@ -502,17 +502,17 @@ knitr::opts_chunk$set(message = FALSE, warning = FALSE, cache = TRUE)
 #' ### Repository: files & folders
 #' [Supplementary information, including code](https://martinbulla.github.io/avian_FID_covid/): the current html document with supplementary informatiion, figures and tables.  
 #'   
-#' [Data](https://github.com/MartinBulla/avian_FID_covid/tree/main/Data): raw data (for their desciption see [READ_ME](Data/READ_ME.txt) and manipulated data (starting with 'DAT_') generated with R-scripts and used in the further analyses
-#' - [model_sim](Data/model_sim): posterior simulations for given models  
-#' - [Pics](Data/Pics): phylopic pictures used in the graphs  
+#' [Data](https://github.com/MartinBulla/avian_FID_covid/tree/main/Data): raw data (for their desciption see [READ_ME](https://github.com/MartinBulla/avian_FID_covid/tree/main/Data/READ_ME.txt) and manipulated data (starting with 'DAT_') generated with R-scripts and used in the further analyses
+#' - [model_sim](https://github.com/MartinBulla/avian_FID_covid/tree/main/Data/model_sim): posterior simulations for given models
+#' - [Pics](https://github.com/MartinBulla/avian_FID_covid/tree/main/Data/Pics): phylopic pictures used in the graphs
 #'  
-#' [R](R/)-scripts used in the analysis; those starting with   
+#' [R](https://github.com/MartinBulla/avian_FID_covid/tree/main/R/)-scripts used in the analysis; those starting with
 #' - "_runRmarkdown.R" generates htmls from the following R-script:  
 #' - "REV_ms_output.R" R-script used to generate the given html document, contains all scripts used to generate the paper outputs, including the display items  
 #' <br />
-#' [Outputs](Outputs/): separate files of all outputs used in the manuscript and this Supplement
+#' [Outputs](https://github.com/MartinBulla/avian_FID_covid/tree/main/Outputs/): separate files of all outputs used in the manuscript and this Supplement
 #'  
-#' [LICENSE](LICENSE): terms of reuse - applicable only after this work is published as a preprint or in a scientific journal, until then the data are not available for reuse.
+#' [LICENSE](https://github.com/MartinBulla/avian_FID_covid/tree/main/LICENSE): terms of reuse - applicable only after this work is published as a preprint or in a scientific journal, until then the data are not available for reuse.
 #'  
 #' ***
 #' 
