9982f9627cb644f3898ae07bc78146da9223be9f
MartinBulla <bulla.mar@gmail.com>
2023-08-08 21:48:01 +0200

    Update _runRmarkdown.R

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -1,5 +1,3 @@
 require(rmarkdown)
 
 rmarkdown::render("R/REV_ms_output.R", output_dir = "Outputs", output_file = "REV_ms_output_v4.html")
-rmarkdown::render("R/REV_analysis.R", output_dir = "Outputs", output_file = "REV_analysis_v2.html")
-rmarkdown::render("R/MET_google-mob_out.R", output_dir = "Outputs", output_file = "EXP_GoogleMobilitty.html")
