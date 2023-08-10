c3113f43e9243270fdb803de0cca8b4c532a7d0a
MartinBulla <bulla.mar@gmail.com>
2023-08-10 12:54:45 +0200

    updated model ass

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -1,4 +1,5 @@
 require(rmarkdown)
 
-rmarkdown::render("R/REV_ms_output.R", output_dir = "Outputs", output_file = "REV_ms_output_v5.html")
+rmarkdown::render("R/REV_ms_output.R", output_dir = "Outputs", output_file = "REV_ms_output_v8.html")
 
+# END
